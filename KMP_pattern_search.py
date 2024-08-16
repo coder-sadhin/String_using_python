4276b1f2480700c021c9d7d56bbebed8bfcfd281
coder-sadhin <codersadhin@gmail.com>
2024-08-16 12:37:24 +0600

    search prefix array

--- KMP_pattern_search.py
+++ KMP_pattern_search.py
@@ -6,3 +6,9 @@ def KMP_pattern_search(pattern, text):
     # create long_prefix_suffix[] that will hold the longest prefix suffix
     # values for pattern
     long_prefix_suffix = [0]*P
+
+    # index for pattern[]
+    j=0
+
+    # preprocess the pattern (caluclate long_prefix_suffix[] array)
+    long_Prefix_Suffix_Array(pattern, P, long_prefix_suffix)
