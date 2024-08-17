a6d0481075b2b4a028cb8b47bc5e91971de91959
coder-sadhin <codersadhin@gmail.com>
2024-08-17 12:40:20 +0600

    vowel pair

---
+++ vowel_pair.py
@@ -0,0 +1,11 @@
+'''
+Question :
+Given a string consisting of English alphabets, the task is to count the number of adjacent pairs of vowels.
+For example :
+Input: str = “abaebio”
+Output: 2
+(a, e) and (i, o) are the only valid pairs.
+Source:A very Common Interview Question.
+
+Time Complexity : The goal is to complete this question in O(n).
+'''
