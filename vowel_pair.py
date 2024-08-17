c1271be1be5c99cba2147b0db4ee8b7040f405ab
coder-sadhin <codersadhin@gmail.com>
2024-08-17 12:40:44 +0600

    is vowel function

--- vowel_pair.py
+++ vowel_pair.py
@@ -9,3 +9,11 @@ Source:A very Common Interview Question.
 
 Time Complexity : The goal is to complete this question in O(n).
 '''
+
+#function to check whether a character is vowel or not
+def is_vowel(character):
+    if character.lower() in ['a', 'e', 'i', 'o', 'u']: 
+        return True
+    else:  
+        return False
+  
