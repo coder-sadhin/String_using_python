b795a42c706ca4d8f7704fb65598e58d30e4d657
coder-sadhin <codersadhin@gmail.com>
2024-08-20 12:54:33 +0600

    permutations

---
+++ check_permutations.py
@@ -0,0 +1,11 @@
+"""
+Question:
+Check Permutation: Given two strings, write a method to decide if
+one is a permutation of the other.
+Source: Cracking the Code Interview 6th Edition Question 1.2
+
+Time Complexity:
+Every letter must be looped which means O(n) time complexity. Then,
+we must check if each letter is in the dictionary which is another
+O(n) time complexity. Overall, the total time complexity is O(n^2).
+"""
