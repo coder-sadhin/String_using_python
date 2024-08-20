662e844f5c7fec24d8c46a535155dbddf9dffd13
coder-sadhin <codersadhin@gmail.com>
2024-08-20 12:58:04 +0600

    finding substring qus

---
+++ finding_substring.py
@@ -0,0 +1,6 @@
+'''
+Question : Print and Count all the substrings of a given string.
+Sample Input : hello
+Sample Output : ['h', 'he', 'hel', 'hell', 'hello', 'e', 'el', 'ell', 'ello', 'l', 'll', 'llo', 'l', 'lo', 'o']
+15
+Source : Hackerrank'''
