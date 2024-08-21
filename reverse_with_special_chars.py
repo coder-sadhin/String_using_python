6901b50b973aae7b07d0c0583ddd3cf9eadcb6f3
coder-sadhin <codersadhin@gmail.com>
2024-08-21 13:04:46 +0600

    reverse with chart condition

--- reverse_with_special_chars.py
+++ reverse_with_special_chars.py
@@ -8,3 +8,13 @@ def reverse(s):
     i = 0
     j = len(s) - 1
     
+    while i < j:
+        if s[i].isalnum() and s[j].isalnum():
+            s[i], s[j] = s[j], s[i]
+            i += 1
+            j -= 1
+        if not s[i].isalnum():
+            i += 1
+        if not s[j].isalnum():
+            j -= 1
+    return ''.join(s)
