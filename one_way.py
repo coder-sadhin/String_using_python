420a7350ed56690ba5c20fe1c462861b6147ce2c
coder-sadhin <codersadhin@gmail.com>
2024-08-21 13:02:42 +0600

    check condition

--- one_way.py
+++ one_way.py
@@ -23,3 +23,27 @@ def is_one_away(str1, str2):
     edit_counter = 0
     i = 0 # str1 index
     j = 0 # str2 index
+
+    # Size difference must be less than 1
+    if abs(len(str1) - len(str2)) > 1:
+        return False
+
+    # Compare strings while counting edits
+    # If letters differ, update counter and compare next letter
+    # In this case, if strings have different sizes increment only index of the longest
+    # Otherwise increment both indexes
+    while i < len(str1) and j < len(str2):
+        if str1[i] != str2[j]:
+            # Only one edit is allowed
+            if edit_counter > 0:
+                return False
+            edit_counter += 1
+
+            if len(str1) > len(str2):
+                i += 1
+                continue
+            elif len(str1) < len(str2):
+                j += 1
+                continue
+        i += 1
+        j += 1
