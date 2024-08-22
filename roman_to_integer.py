0796ec2562db4ef5d616b90a5f3c96d7a02b78a8
coder-sadhin <codersadhin@gmail.com>
2024-08-22 13:06:07 +0600

    roman

---
+++ roman_to_integer.py
@@ -0,0 +1,19 @@
+'''
+Question:
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M and their values are 1,5,10,50,100,500,1000 respectively.Given a roman numeral,convert it to an integer.Input is guaranteed to be within the range from 1 to 3999.
+
+Example 1:
+Input : 'XI'
+Output : 11
+
+Example 2:
+Input : 'CCCXL'
+Output : 340
+
+Source: Interview question.
+
+Time Complexity : O(n)
+Space Complexity : O(1)
+
+'''
+
