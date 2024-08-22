266edba54587a68cd60382c834ce1659eb9a3529
coder-sadhin <codersadhin@gmail.com>
2024-08-22 13:08:27 +0600

    roman to string

---
+++ roman_to_string.py
@@ -0,0 +1,9 @@
+"""
+Given two strings, A and B.
+
+A shift on A consists of taking string A and moving the leftmost character to the rightmost position. For example, if A = 'abcde', then it will be 'bcdea' after one shift on A. Return True if and only if A can become B after some number of shifts on A.
+
+:type A: str
+:type B: str
+:rtype: bool
+"""
